28377d6396f1f65f8d814eb27b8f4dfa1c7dc4bd
akmhmgc <akimuhamaguchi@icloud.com>
2022-05-05 16:13:46 +0900

    Initiallize firebase

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+
+import {
+  initializeApp
+} from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: process.env["apiKey"],
+  authDomain: process.env["authDomain"],
+  projectId: process.env["projectId"],
+  storageBucket: process.env["storageBucket"],
+  messagingSenderId: process.env["messagingSenderId"],
+  appId: process.env["appId"]
+};
+
+initializeApp(firebaseConfig)
